39c54ebf09dd03350551ec8f8ab03eb8469ca8f8
Lucas Werkmeister <mail@lucaswerkmeister.de>
2025-03-21 17:30:12 +0100

    Don’t use import.meta.dirname
    I’ve changed my mind, I want this to work on Node 18 (Debian Bookworm)
    after all.

--- webapp-serverside-express-guestbook/app.js
+++ webapp-serverside-express-guestbook/app.js
@@ -1,6 +1,7 @@
 import createError from 'http-errors';
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 
@@ -9,15 +10,17 @@ import usersRouter from './routes/users.js';
 
 var app = express();
 
+const basePath = path.dirname(fileURLToPath(import.meta.url));
+
 // view engine setup
-app.set('views', path.join(import.meta.dirname, 'views'));
+app.set('views', path.join(basePath, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(import.meta.dirname, 'public')));
+app.use(express.static(path.join(basePath, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
